42ae3108a05aba60f2952b6e4f25f8f394e9d826
geanes <glenn.eanes@gmail.com>
2015-02-20 15:33:04 -0600

    Add lower_names function

--- R/cases.R
+++ R/cases.R
@@ -12,6 +12,15 @@ assertthat::on_failure(dots_exist) <- function(call, env){
   paste0("columns must be specified for standard evaluation")
 }
 
+valid_columns <- function(data, dots){
+  all(sapply(dots, function(x) as.character(x$expr) %in% names(data)))
+}
+assertthat::on_failure(valid_columns) <- function(call, env){
+  paste0("columns specified do not match data")
+}
+
+#----------------------------------------------------------------------------
+
 lower_factors <- function(.data, ..., case = "lower"){
   dots = lazyeval::lazy_dots(...)
   if (length(dots) == 0) dots <- names(.data)[which(sapply(.data, is.factor))]
@@ -23,6 +32,7 @@ lower_factors_ <- function(.data, ..., .dots, case = "lower"){
   cols <- lazyeval::lazy_eval(alldots, .data)
   assertthat::assert_that(is.data.frame(.data))
   assertthat::assert_that(dots_exist(alldots))
+  assertthat::assert_that(valid_columns(.data, alldots))
   assertthat::assert_that(all_factors(cols))
   assertthat::assert_that(is.character(case))
   
@@ -45,6 +55,24 @@ lower_factor_ <- function(.data, case){
 
 # ------------------------------------------------------------------------------
 
-lower_names <- function(.data, cols = names(.data), case = "lower"){
-  names(.data) <- tolower(names(.data))
+lower_names <- function(.data, ..., case = "lower"){
+  dots <- lazyeval::lazy_dots(...)
+  if (length(dots) == 0) dots <- names(.data)
+  lower_names_(.data, .dots = dots, case = case)
+}
+
+
+lower_names_ <- function(.data, ..., .dots, case = "lower"){
+  alldots <- lazyeval::all_dots(.dots, ...)
+  assertthat::assert_that(is.data.frame(.data))
+  assertthat::assert_that(valid_columns(.data, alldots))
+  assertthat::assert_that(dots_exist(alldots))
+  assertthat::assert_that(is.character(case))
+  cols <- sapply(alldots, function(x) which(names(.data) == as.character(x$expr)))
+  if (case == "upper"){
+    names(.data)[cols] <- toupper(names(.data)[cols])
+  } else {
+    names(.data)[cols] <- tolower(names(.data)[cols])
+  }
+  return(.data)
 }
