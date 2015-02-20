3f0ce393e9bfad39197f18175bd2cee05d50510d
geanes <glenn.eanes@gmail.com>
2015-02-20 17:17:03 -0600

    Update package documentation

---
+++ R/bioanth.R
@@ -0,0 +1,16 @@
+#' bioanth: Datasets useful for biological and forensic anthropology.
+#' 
+#' \code{bioanth} provides datasets useful for biological and forensic
+#' anthropology. In addition, it includes helper functions for working with
+#' dataframes.
+#' 
+#' @details
+#' This package contains the following datasets:
+#' \itemize{
+#'   \item Howell's craniometrics
+#'   \item Goldman
+#' }
+#' 
+#' @docType package
+#' @name bioanth
+NULL
