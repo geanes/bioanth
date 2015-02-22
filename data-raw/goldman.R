769c65b5e4944dd15504d9ba5a8858838cd806c5
geanes <glenn.eanes@gmail.com>
2015-02-22 00:32:03 -0600

    Replace non-ASCII characters in goldman

--- data-raw/goldman.R
+++ data-raw/goldman.R
@@ -9,6 +9,15 @@ goldman <- read.csv(raw, stringsAsFactors=FALSE)
 # Initial munge ----------------------------------------------------------
 goldman %<>% select(-Element., -Metrics., -Derived.)
 
+# Replace non-ASCII ------------------------------------------------------
+goldman$NOTE <- gsub("\xa7", "\u00df", goldman$NOTE)
+goldman$NOTE <- gsub("\x87", "\u00e1", goldman$NOTE)
+goldman$NOTE <- gsub("\xa1", "\u00b0", goldman$NOTE)
+goldman$NOTE <- gsub("\xc9", "...", goldman$NOTE)
+
+
+
+
 # Make factors -----------------------------------------------------------
 goldman$Inst %<>% factor
 goldman$Sex %<>% factor(labels = c("M", "F", "M?", "F?"), levels = c("0", "1", "0?", "1?"))
